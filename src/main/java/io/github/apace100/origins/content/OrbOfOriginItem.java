052058c6cd499647c123c7b141208d098e271169
eggohito <66972816+eggohito@users.noreply.github.com>
2024-07-11 01:00:52 +0800

    Generalized and renamed origin targets component to item origins Now any item (except for items that have their own use actions...) can either set or open the choose origin screen if given the item origins component

--- src/main/java/io/github/apace100/origins/content/OrbOfOriginItem.java
+++ src/main/java/io/github/apace100/origins/content/OrbOfOriginItem.java
@@ -1,61 +1,21 @@
 package io.github.apace100.origins.content;
 
-import io.github.apace100.origins.origin.Origin;
-import io.github.apace100.origins.util.OriginTargetComponent;
-import net.minecraft.entity.player.PlayerEntity;
+import io.github.apace100.origins.component.item.ItemOriginsComponent;
+import io.github.apace100.origins.registry.ModDataComponentTypes;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.text.Text;
 import net.minecraft.util.*;
-import net.minecraft.world.World;
-
-import java.util.List;
 
 public class OrbOfOriginItem extends Item {
 
     public OrbOfOriginItem() {
-        this(new Settings().maxCount(1).rarity(Rarity.RARE));
+        this(new Settings()
+            .maxCount(1)
+            .rarity(Rarity.RARE)
+            .component(ModDataComponentTypes.ORIGIN, ItemOriginsComponent.DEFAULT));
     }
 
     public OrbOfOriginItem(Settings settings) {
         super(settings);
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-
-        ItemStack stack = user.getStackInHand(hand);
-        OriginTargetComponent.applyTargets(stack, user);
-
-        stack.decrementUnlessCreative(1, user);
-        return TypedActionResult.consume(stack);
-
-    }
-
-    @Override
-    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
-
-        OriginTargetComponent.getTargetsAsMap(stack).forEach((layer, origin) -> {
-
-            String baseKey = "item.origins.orb_of_origin.layer_";
-            Object[] args;
-
-            if (origin == Origin.EMPTY) {
-                baseKey += "generic";
-                args = new Object[] { Text.translatable(layer.getTranslationKey()) };
-            } else {
-                baseKey += "specific";
-                args = new Object[] {
-                    Text.translatable(layer.getTranslationKey()),
-                    Text.translatable(origin.getOrCreateNameTranslationKey())
-                };
-            }
-
-            tooltip.add(Text.translatable(baseKey, args));
-
-        });
-
     }
 
 }
--- src/main/java/io/github/apace100/origins/registry/ModDataComponentTypes.java
+++ src/main/java/io/github/apace100/origins/registry/ModDataComponentTypes.java
@@ -1,24 +1,20 @@
 package io.github.apace100.origins.registry;
 
 import io.github.apace100.origins.Origins;
-import io.github.apace100.origins.util.OriginTargetComponent;
+import io.github.apace100.origins.component.item.ItemOriginsComponent;
 import net.minecraft.component.ComponentType;
-import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ModDataComponentTypes {
 
-    public static final ComponentType<List<OriginTargetComponent>> ORIGIN_TARGET = ComponentType.<List<OriginTargetComponent>>builder()
-        .codec(OriginTargetComponent.CODEC.listOf())
-        .packetCodec(PacketCodecs.collection(ArrayList::new, OriginTargetComponent.PACKET_CODEC))
+    public static final ComponentType<ItemOriginsComponent> ORIGIN = ComponentType.<ItemOriginsComponent>builder()
+        .codec(ItemOriginsComponent.CODEC)
+        .packetCodec(ItemOriginsComponent.PACKET_CODEC)
         .build();
 
     public static void register() {
-        Registry.register(Registries.DATA_COMPONENT_TYPE, Origins.identifier("targets"), ORIGIN_TARGET);
+        Registry.register(Registries.DATA_COMPONENT_TYPE, Origins.identifier("origin"), ORIGIN);
     }
 
 }
